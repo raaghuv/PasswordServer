573296fd0238af731777c381307161b60318a314
ShaneESchwartz <shane.e.schwartz32@gmail.com>
2026-04-09 12:36:13 -0400

    shane sub repo

---
+++ Shane/spring_files/demo/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.User;
+import com.example.demo.model.UserRepository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    public void createUser(@RequestBody User user) {
+        userRepository.save(user);
+    }
+
+}
